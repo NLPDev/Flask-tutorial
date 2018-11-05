8b5e5a742994c171609826e2b98fb976f76ac5ec
ML expert <vasile123andronic@gmail.com>
2018-11-05 08:14:12 -0800

    message flash

--- hello.py
+++ hello.py
@@ -1,19 +1,23 @@
-from flask import Flask, redirect, url_for, render_template, request, abort
+from flask import Flask, flash, redirect, render_template, request, url_for
 app = Flask(__name__)
+app.secret_key = 'random string'
 
 @app.route('/')
 def index():
    return render_template('index.html')
 
-@app.route('/login',methods = ['POST', 'GET'])
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
+   error = None
+   
    if request.method == 'POST':
-      if request.form['username'] == 'admin' :
-         return redirect(url_for('success'))
+      if request.form['username'] != 'admin' or \
+         request.form['password'] != 'admin':
+         error = 'Invalid username or password. Please try again!'
       else:
-         abort(401)
-   else:
-      return redirect(url_for('index'))
+         flash('You were successfully logged in')
+         return redirect(url_for('index'))
+   return render_template('login.html', error = error)
 
 @app.route('/success')
 def success():
