4c3f453db07f98955fb421cbd023736439251378
ML expert <vasile123andronic@gmail.com>
2018-11-05 08:48:19 -0800

    upload file

--- hello.py
+++ hello.py
@@ -1,27 +1,18 @@
-from flask import Flask, flash, redirect, render_template, request, url_for
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
 app = Flask(__name__)
-app.secret_key = 'random string'
 
-@app.route('/')
-def index():
-   return render_template('index.html')
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
 
-@app.route('/login', methods = ['GET', 'POST'])
-def login():
-   error = None
-   
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploaded_file():
    if request.method == 'POST':
-      if request.form['username'] != 'admin' or \
-         request.form['password'] != 'admin':
-         error = 'Invalid username or password. Please try again!'
-      else:
-         flash('You were successfully logged in')
-         return redirect(url_for('index'))
-   return render_template('login.html', error = error)
-
-@app.route('/success')
-def success():
-   return 'logged in successfully'
-
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
+		
 if __name__ == '__main__':
    app.run(debug = True)
