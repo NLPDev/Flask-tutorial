06ae6cadc29910c22f993efc144eade85abae40d
ML expert <vasile123andronic@gmail.com>
2018-11-08 09:58:17 -0800

    sijax example

--- hello.py
+++ hello.py
@@ -1,48 +1,27 @@
-from flask import Flask, request, flash, url_for, redirect, render_template
-from flask_sqlalchemy import SQLAlchemy
+import os
+from flask import Flask, g
+from flask_sijax import sijax
 
-import sqlite3 as sql
+path = os.path.join('.', os.path.dirname(__file__), 'static/js/sijax/')
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///students.sqlite3'
-app.config['SECRET_KEY'] = "random string"
-
-db = SQLAlchemy(app)
-
-class students(db.Model):
-   id = db.Column('student_id', db.Integer, primary_key = True)
-   name = db.Column(db.String(100))
-   city = db.Column(db.String(50))  
-   addr = db.Column(db.String(200))
-   pin = db.Column(db.String(10))
-
-   def __init__(self, name, city, addr,pin):
-      self.name = name
-      self.city = city
-      self.addr = addr
-      self.pin = pin
-
+app.config['SIJAX_STATIC_PATH'] = path
+app.config['SIJAX_JSON_URI'] = '/static/js/sijax/json2.js'
+flask_sijax.Sijax(app)
 
 @app.route('/')
-def show_all():
-   return render_template('show_all.html', students = students.query.all() )
-
-@app.route('/new', methods = ['GET', 'POST'])
-def new():
-   if request.method == 'POST':
-      if not request.form['name'] or not request.form['city'] or not request.form['addr']:
-         flash('Please enter all the fields', 'error')
-      else:
-         student = students(request.form['name'], request.form['city'], request.form['addr'], request.form['pin'])
-         
-         db.session.add(student)
-         db.session.commit()
-         
-         flash('Record was successfully added')
-         return redirect(url_for('show_all'))
-   return render_template('new.html')
-
+def index():
+   return 'Index'
+	
+@flask_sijax.route(app, '/hello')
+def hello():
+   def say_hi(obj_response):
+      obj_response.alert('Hi there!')
+   if g.sijax.is_sijax_request:
+      # Sijax request detected - let Sijax handle it
+      g.sijax.register_callback('say_hi', say_hi)
+      return g.sijax.process_request()
+   return _render_template('sijaxexample.html')
 
 if __name__ == '__main__':
-   db.create_all()
    app.run(debug = True)
