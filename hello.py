20cf440cea17dcdaaf3c199b3f8c518a72d8082b
ML expert <vasile123andronic@gmail.com>
2018-11-17 11:50:15 -0800

    werkzeug route

--- hello.py
+++ hello.py
@@ -1,27 +1,41 @@
-import os
-from flask import Flask, g, render_template
-import flask_sijax
+# import os
+# from flask import Flask, g, render_template
+# import flask_sijax
 
-path = os.path.join('.', os.path.dirname(__file__), 'static/js/sijax/')
+# path = os.path.join('.', os.path.dirname(__file__), 'static/js/sijax/')
+# app = Flask(__name__)
+
+# app.config['SIJAX_STATIC_PATH'] = path
+# app.config['SIJAX_JSON_URI'] = '/static/js/sijax/json2.js'
+# flask_sijax.Sijax(app)
+
+# @app.route('/')
+# def index():
+#    return 'Index'
+	
+# @flask_sijax.route(app, '/hello')
+# def hello():
+#    def say_hi(obj_response):
+#       obj_response.alert('Hi there!')
+#    if g.sijax.is_sijax_request:
+#       # Sijax request detected - let Sijax handle it
+#       g.sijax.register_callback('say_hi', say_hi)
+#       return g.sijax.process_request()
+#    return render_template('sijaxexample.html')
+
+# if __name__ == '__main__':
+#    app.run(debug = True)
+
+from flask import Flask
 app = Flask(__name__)
 
-app.config['SIJAX_STATIC_PATH'] = path
-app.config['SIJAX_JSON_URI'] = '/static/js/sijax/json2.js'
-flask_sijax.Sijax(app)
+@app.route('/flask')
+def hello_flask():
+   return 'Hello Flask'
 
-@app.route('/')
-def index():
-   return 'Index'
-	
-@flask_sijax.route(app, '/hello')
-def hello():
-   def say_hi(obj_response):
-      obj_response.alert('Hi there!')
-   if g.sijax.is_sijax_request:
-      # Sijax request detected - let Sijax handle it
-      g.sijax.register_callback('say_hi', say_hi)
-      return g.sijax.process_request()
-   return render_template('sijaxexample.html')
+@app.route('/python/')
+def hello_python():
+   return 'Hello Python'
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run()
