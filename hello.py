b77c2ef68aa1180d4ea1c7174617ffc501c20964
ML expert <vasile123andronic@gmail.com>
2018-11-17 13:56:40 -0800

    template data

--- hello.py
+++ hello.py
@@ -26,16 +26,18 @@
 # if __name__ == '__main__':
 #    app.run(debug = True)
 
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/flask')
-def hello_flask():
-   return 'Hello Flask'
+@app.route('/')
+def student():
+   return render_template('student.html')
 
-@app.route('/python/')
-def hello_python():
-   return 'Hello Python'
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug = True)
