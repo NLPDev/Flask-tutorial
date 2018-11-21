7ad7388d29194dc94e25cae7bcbc8fb8f8ce407b
ML expert <vasile123andronic@gmail.com>
2018-11-21 11:44:24 -0800

    pre message

--- flask.py
+++ flask.py
@@ -6,19 +6,19 @@ app.secret_key = 'random string'
 def index():
    return render_template('index.html')
 
-# @app.route('/login', methods = ['GET', 'POST'])
-# def login():
-#    error = None
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+   error = None
    
-#    if request.method == 'POST':
-#       if request.form['username'] != 'admin' or \
-#          request.form['password'] != 'admin':
-#          error = 'Invalid username or password. Please try again!'
-#       else:
-#          flash('You were successfully logged in')
-#          return redirect(url_for('index'))
+   if request.method == 'POST':
+      if request.form['username'] != 'admin' or \
+         request.form['password'] != 'admin':
+         error = 'Invalid username or password. Please try again!'
+      else:
+         flash('You were successfully logged in')
+         return redirect(url_for('index'))
 			
-#    return render_template('login.html', error = error)
+   return render_template('login.html', error = error)
 
 if __name__ == "__main__":
    app.run(debug = True)
--- hello.py
+++ hello.py
@@ -13,15 +13,6 @@ flask_sijax.Sijax(app)
 def index():
    return 'Index'
 	
-@flask_sijax.route(app, '/hello')
-def hello():
-   def say_hi(obj_response):
-      obj_response.alert('Hi there!')
-   if g.sijax.is_sijax_request:
-      # Sijax request detected - let Sijax handle it
-      g.sijax.register_callback('say_hi', say_hi)
-      return g.sijax.process_request()
-   return render_template('sijaxexample.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
